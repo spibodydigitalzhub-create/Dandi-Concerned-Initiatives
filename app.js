985c2f1fc85d47ff5d9dad9e985c2f3628d74f3f
Spibody Digitalz Hub <spibodydigitalzhub@gmail.com>
2026-04-24 15:52:54 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+function toggleMenu() {
+  document.getElementById("navMenu").classList.toggle("active");
+}
+const openWhatsapp = document.getElementById("openWhatsapp");
+const closeWhatsapp = document.getElementById("closeWhatsapp");
+const waPopup = document.getElementById("waPopup");
+const startWhatsapp = document.getElementById("startWhatsapp");
+const waName = document.getElementById("waName");
+
+if (openWhatsapp) {
+  openWhatsapp.addEventListener("click", function(e) {
+    e.preventDefault();
+    waPopup.classList.add("active");
+  });
+}
+
+if (closeWhatsapp) {
+  closeWhatsapp.addEventListener("click", function() {
+    waPopup.classList.remove("active");
+  });
+}
+
+if (startWhatsapp) {
+  startWhatsapp.addEventListener("click", function() {
+    const name = waName.value.trim();
+
+    if (name === "") {
+      waName.style.border = "1px solid red";
+      return;
+    }
+
+    const phone = "2348063182224"; 
+    const message = `Hello, my name is ${name}. I want to support your NGO.`;
+    const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+    window.open(url, "_blank");
+    waPopup.classList.remove("active");
+  });
+}
