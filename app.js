d3121f19b49df0d7590dc792568272c457af4121
Spibody Digitalz Hub <spibodydigitalzhub@gmail.com>
2026-04-24 18:34:54 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -37,3 +37,17 @@ if (startWhatsapp) {
     waPopup.classList.remove("active");
   });
 }
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+galleryImages.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
